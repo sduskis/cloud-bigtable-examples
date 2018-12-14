7f2ae633ee5106da78c40fd65796c684767cf2c4
Solomon Duskis <sduskis@google.com>
2018-12-14 12:57:35 -0500

    Upgrading to cloud-bigtable 1.7.0

--- quickstart/thirdparty/ruby/hbase/admin.rb
+++ quickstart/thirdparty/ruby/hbase/admin.rb
@@ -250,7 +250,6 @@ module Hbase
     # Drops a table
     def drop(table_name)
       tableExists(table_name)
-      raise ArgumentError, "Table #{table_name} is enabled. Disable it first." if enabled?(table_name)
 
       @admin.deleteTable(org.apache.hadoop.hbase.TableName.valueOf(table_name))
     end
